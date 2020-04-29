3f66562c662e6c5cee9ddddc5ca8d7a7279a9f75
pavlo_kosiak <pavlo_kosiak@epam.com>
2020-04-29 21:11:36 +0300

    - pass "The throw method receive only valid parameters" test

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ class DartsGame {
     this._players = this._createPlayers(players);
     this._currentPlayerIndex = 0;
     this._throwCount = 0;
+    this._validScores = [1, 18, 4, 13, 6, 10, 15, 2, 17, 3, 19, 7, 16, 8, 11, 14, 9, 12, 5, 20];
+    this._validFactor = [1, 2, 3];
   }
 
   _createPlayers = playerNames => playerNames.map(playerName => {
@@ -14,17 +16,12 @@ class DartsGame {
   });
 
   score = playerIndex => {
-    if (!Number.isInteger(playerIndex)) {
-      throw new Error('Type of index must be an int!')
-    }
-
+    this._validateScoreParams(playerIndex);
     return this._players[playerIndex].currentPoints;
   };
 
   throw = (score, factor) => {
-    if (!Number.isInteger(score) || !Number.isInteger(factor)) {
-      throw new Error('All parameters must be an int!')
-    }
+    this._validateThrowParams(score, factor);
 
     const lastThrowPoints = score * factor;
     this._throwCount++;
@@ -34,6 +31,25 @@ class DartsGame {
     }
 
     this._players[this._currentPlayerIndex].currentPoints -= lastThrowPoints;
+  };
+
+  _validateThrowParams = (score, factor) => {
+    if (!Number.isInteger(score) || !Number.isInteger(factor)) {
+      throw new Error('All parameters must be an int!');
+    }
+
+    const areParamsValid = !!this._validScores.filter(validScore => score === validScore).length
+      && !!this._validFactor.filter(validScore => factor === validScore).length;
+
+    if (!areParamsValid) {
+      throw new Error('The throw parameters must be valid!');
+    }
+  };
+
+  _validateScoreParams = playerIndex => {
+    if (!Number.isInteger(playerIndex)) {
+      throw new Error('Type of index must be an int!')
+    }
   };
 }
 
