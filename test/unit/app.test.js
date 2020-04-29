d3414a1cad5ab8edd359a87d93ead9d43fe15393
pavlo_kosiak <pavlo_kosiak@epam.com>
2020-04-29 22:09:05 +0300

    - add describes

--- test/unit/app.test.js
+++ test/unit/app.test.js
@@ -2,25 +2,33 @@ const DartsGame = require('./../../app');
 let game;
 
 describe('Test DartsGame', () => {
-  beforeEach(() => {
-    game = new DartsGame(['Ivan', 'Petr']);
-  });
-
   test('Handle clean game', () => {
     expect(DartsGame).toBe(DartsGame);
+  });
+});
+
+describe('Test the score method', () => {
+  beforeEach(() => {
+    game = new DartsGame(['Ivan', 'Petr']);
   });
 
   test('Receive points for first player', () => {
     expect(game.score(0)).toBe(301);
   });
 
+  test('The score method receive only valid type of data', () => {
+    expect(() => game.score('0')).toThrow('Type of index must be an int!');
+  });
+});
+
+describe('Test the throw method', () => {
+  beforeEach(() => {
+    game = new DartsGame(['Ivan', 'Petr']);
+  });
+
   test('The first player can make the first throw', () => {
     game.throw(9, 3);
     expect(game.score(0)).toBe(274);
-  });
-
-  test('The score method receive only valid type of data', () => {
-    expect(() => game.score('0')).toThrow('Type of index must be an int!');
   });
 
   test('The throw method receive only valid types of data', () => {
