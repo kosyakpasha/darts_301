bc98227e1b9f325d811f4d68295e5db2266c1378
pavlo_kosiak <pavlo_kosiak@epam.com>
2020-04-29 17:26:34 +0300

    - pass "Receive points for first player" test

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 class DartsGame {
+  constructor(players) {
+    this._players = this._createPlayers(players);
+  }
 
+  _createPlayers = playerNames => playerNames.map(playerName => {
+    return {
+      name: playerName,
+      currentPoints: 301,
+      isWinner: false,
+    }
+  });
+
+  score = playerIndex => this._players[playerIndex].currentPoints;
 }
 
 module.exports = DartsGame;
