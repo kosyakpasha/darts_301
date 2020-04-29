299aae8d2378ea63e843f395bbda98002bb0a489
pavlo_kosiak <pavlo_kosiak@epam.com>
2020-04-29 17:35:47 +0300

    - fail "The first player can make the first throw" test

--- test/unit/app.test.js
+++ test/unit/app.test.js
@@ -13,4 +13,9 @@ describe('Test DartsGame', () => {
   test('Receive points for first player', () => {
     expect(game.score(0)).toBe(301);
   });
+
+  test('The first player can make the first throw', () => {
+    game.throw(9, 3);
+    expect(game.score(0)).toBe(274);
+  });
 });
